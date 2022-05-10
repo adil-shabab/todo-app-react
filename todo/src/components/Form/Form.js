04aa9fc1e2082fc5d0001d153af6300f26ab10b8
Adil Shabab <adilshabab2004@gmail.com>
2022-05-10 15:28:50 +0530

    Todolist file created

--- todo/src/components/Form/Form.js
+++ todo/src/components/Form/Form.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './Form.css'
 
 function Form() {
   return (
---
+++ todo/src/components/TodoList/TodoList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TodoList() {
+  return (
+    <div>TodoList</div>
+  )
+}
+
+export default TodoList
