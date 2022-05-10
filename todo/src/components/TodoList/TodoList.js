4fc79f8dd8da88de2835f6493cfade044996c17a
Adil Shabab <adilshabab2004@gmail.com>
2022-05-10 20:38:53 +0530

    handle for delete complted

--- todo/src/components/TodoList/TodoList.js
+++ todo/src/components/TodoList/TodoList.js
@@ -2,6 +2,21 @@ import React from "react";
 import "./TodoList.css";
 
 function TodoList({ todos, setTodos }) {
+    const handleComplete=(todo)=>{
+        setTodos(
+            todos.map((item)=>{
+                if(item.id===todo.id){
+                    return {...item, completed: !item.completed}
+                }
+                return item
+            })
+        )
+        console.log(todos)
+    }
+
+    const handleDelete=({id})=>{
+        setTodos(todos.filter((todo)=>todo.id !==id))
+    }
   return (
   <div className="list">
       <ul>
@@ -9,9 +24,10 @@ function TodoList({ todos, setTodos }) {
         return(
         <li key={index}>
           {todo.title} 
-          
+
+          <i onClick={()=> handleComplete(todo)}  class="fa-solid fa-circle-check"></i>
           <i className="fa-solid fa-pen-to-square"></i>
-          <i  className="fa-solid fa-trash-can"></i>
+          <i onClick={()=>handleDelete(todo)} className="fa-solid fa-trash-can"></i>
         </li>
         )
       })}
