b4c8fc465ac9d03fefb93cc55c9eee00ec9729c4
Adil Shabab <adilshabab2004@gmail.com>
2022-05-10 15:22:59 +0530

    header completed

--- todo/src/components/Header/Header.js
+++ todo/src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import './Header.css'
 
 function Header() {
   return (
     <div className='container'>
-
+        <h2>Todo App</h2>
+        <input type="text" />
     </div>
   )
 }
