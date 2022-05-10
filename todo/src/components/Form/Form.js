431b4c298ac313a1345c6046515463efdcae5b0f
Adil Shabab <adilshabab2004@gmail.com>
2022-05-10 15:24:56 +0530

    form js file created

--- todo/src/components/Form/Form.js
+++ todo/src/components/Form/Form.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
 function Form() {
   return (
-    <div>Form</div>
-  )
+    <div className="todo-container d-flex flex-column justify-content-center align-items-center">
+      <div className="shadow p-3 d-flex flex-column justify-content-center align-items-center">
+        <h1>Todo App</h1>
+        <div>
+          <form action="">
+            <input
+              className="box-shadow"
+              onChange={(e) => {
+                setInput(e.target.value);
+              }}
+              value={input}
+              type="text"
+            />
+            <button onClick={addTask} className="btn shadow ms-3">
+              Add
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Form
+export default Form;
